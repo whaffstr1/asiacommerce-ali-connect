7aa6f321daa01e2d5e3f6149ceba638eb4733195
whaffstr1 <whaffstr1@gmail.com>
2022-02-20 10:03:31 +0700

    Menambahkan fungsi utama multiple packages pada main file ali-connect

--- ali-connect.php
+++ ali-connect.php
@@ -2498,3 +2498,75 @@ if(is_plugin_active('woocommerce/woocommerce.php')){
         );
     }
 }
+
+if (is_multisite()) {
+    $activePlugins = get_site_option('active_sitewide_plugins', array());
+    $activePlugins = array_keys($activePlugins);
+} else {
+    $activePlugins = apply_filters('active_plugins', get_option('active_plugins'));
+}
+
+/**
+ * Check if WooCommerce is active.
+ */
+if (in_array('woocommerce/woocommerce.php', $activePlugins)) {
+
+    if (!class_exists('Academe_Multiple_Packages')) {
+        // Include the main class.
+        // We will keep classes each defined in their own files.
+        include_once(dirname(__FILE__) . '/classes/Academe_Multiple_Packages.php');
+    }
+
+    // Add the filter to generate the packages.
+    // At the moment, this plugin will discard any packages already created and then
+    // generate its own from scratch. A future enhancement would see this plugin
+    // taking the existing packages, and perhaps splitting them down further if
+    // necessary, then adding the linking meta fields to the result.
+    add_filter(
+        'woocommerce_cart_shipping_packages',
+        array(Academe_Multiple_Packages::get_instance(), 'generate_packages')
+    );
+
+    // This action allows plugins to add order item meta to shipping.
+    add_action(
+        'woocommerce_add_shipping_order_item',
+        array(Academe_Multiple_Packages::get_instance(), 'link_shipping_line_item'),
+        10, 3
+    );
+
+    // Add shipping line meta fields to the shipping line in the order API.
+    add_filter(
+        'woocommerce_api_order_response',
+        array(Academe_Multiple_Packages::get_instance(), 'api_show_shipping_line_meta'),
+        10, 4
+    );
+
+    /**
+     * The settings are needed only when in the admin area.
+     */
+    if (is_admin()) {
+        /**
+         * Define the shipping method.
+         */
+        function academe_wc_multiple_packages_init()
+        {
+            if (!class_exists('Academe_Multiple_Packages_Settings')) {
+                // Include the settings and create a new instance.
+                require_once(dirname(__FILE__) . '/classes/Academe_Multiple_Packages_Settings.php');
+            }
+        }
+        add_action('woocommerce_shipping_init', 'academe_wc_multiple_packages_init');
+
+        /**
+         * Add the shipping method to the WC list of methods.
+         * It is not strictly a shipping method itself, but a tool for grouping other
+         * shipping methods.
+         */
+        function academe_add_wc_multiple_packages($methods)
+        {
+            $methods[] = 'Academe_Multiple_Packages_Settings';
+            return $methods;
+        }
+        add_filter('woocommerce_shipping_methods', 'academe_add_wc_multiple_packages');
+    }
+}
